d93011a95d5914e4312c4689312e49433c6e63a4
IltisGraph <rziamtsou@gmail.com>
2023-05-26 20:53:04 +0200

    made start screen size better for smaller screens. Made the UI od MainMenuScreen.java react better to screen resizes by using a ExtendedViewport. Also fixed the problem with the color change of the "Play" and the "Exit" button by using the World coordinates (given by viewport.unproject) rather than the pixel coordinates.

--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -9,7 +9,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setWindowedMode(1500, 1000);
+		config.setWindowedMode(800, 450);
 
 		config.setResizable(true);
 		config.setTitle("RPG TEST");
